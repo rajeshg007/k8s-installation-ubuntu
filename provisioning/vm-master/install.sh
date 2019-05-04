fe2f458c42740800b28cd15c6fc51889b4589ecc
rajeshg007 <rajeshg1992@gmail.com>
2019-05-04 21:15:41 +0530

    Rajeshg007 patch 1 (#1)
    * Update install.sh
    * network components

--- provisioning/vm-master/install.sh
+++ provisioning/vm-master/install.sh
@@ -2,30 +2,35 @@
 
 figlet MASTER
 
-echo "[TASK 2] Start master"
+echo "[TASK 1] Start master"
 kubeadm init --ignore-preflight-errors all --pod-network-cidr=10.244.0.0/16 --token-ttl 0
 
-echo "[TASK 3] Install Calico"
- kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml 
+echo "[TASK 2] Install kubeconfig and additional network components"
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+echo 'export KUBECONFIG=$HOME/.kube/config' >> $HOME/.bashrc
+echo "export kubever=$(kubectl version | base64 | tr -d '\n')" >> $HOME/.bashrc
+export KUBECONFIG=$HOME/.kube/config
+export kubever=$(kubectl version | base64 | tr -d '\n')
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+systemctl restart docker && systemctl restart kubelet
 
+echo "[TASK 3] Install Calico"
+kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml 
 
 echo "[TASK 4] Display PODS"
 kubectl get pods --all-namespaces
 
-
-echo "[TASK 5] Install kubeconfig"
-$ mkdir -p $HOME/.kube
-$ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-$ sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-echo "[TASK 6] Install Dashboard"
+echo "[TASK 5] Install Dashboard"
 kubectl apply -f kubernetes-dashboard.yaml
 kubectl apply -f kubernetes-dashboard-rbac.yaml
 
-echo "[TASK 7] Display All Services"
+echo "[TASK 6] Display All Services"
 kubectl get services -n kube-system 
 
 
+echo "[TASK 7] Setup NFS"
 figlet NFS
 apt-get install -y nfs-kernel-server
 apt-get install -y nfs-common
