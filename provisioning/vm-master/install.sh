8d556bea264f2e85b1fe32ff154788f5316ed3d6
rajeshg007 <rajeshg1992@gmail.com>
2019-05-04 23:31:33 +0530

    Update install.sh

--- provisioning/vm-master/install.sh
+++ provisioning/vm-master/install.sh
@@ -5,7 +5,7 @@ figlet MASTER
 echo "[TASK 1] Start master"
 kubeadm init --ignore-preflight-errors all --pod-network-cidr=10.244.0.0/16 --token-ttl 0
 
-echo "[TASK 2] Install kubeconfig and additional network components"
+echo "[TASK 2] Install kubeconfig"
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
@@ -13,11 +13,11 @@ echo 'export KUBECONFIG=$HOME/.kube/config' >> $HOME/.bashrc
 echo "export kubever=$(kubectl version | base64 | tr -d '\n')" >> $HOME/.bashrc
 export KUBECONFIG=$HOME/.kube/config
 export kubever=$(kubectl version | base64 | tr -d '\n')
+
+echo "[TASK 3] Install Calico and needed network tools"
+kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
 systemctl restart docker && systemctl restart kubelet
-
-echo "[TASK 3] Install Calico"
-kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml 
 
 echo "[TASK 4] Display PODS"
 kubectl get pods --all-namespaces
